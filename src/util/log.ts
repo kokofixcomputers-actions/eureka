5da32b71f4d9aa12152191738ee85ca955097b3d
kokofixcomputers <github@kokofixcomputers.serv00.net>
2024-05-07 20:07:15 -0700

    make logs error red

--- src/util/log.ts
+++ src/util/log.ts
@@ -27,7 +27,7 @@ export function warn (...params: unknown[]) {
 export function error (...params: unknown[]) {
     console.error(
         '%c🚨 Eureka',
-        ` background-color: #fecd00; border-radius: 1rem; margin-right: 0.25rem; padding: 0 0.5rem; color: #271919;`,
+        ` background-color: #FE4B00; border-radius: 1rem; margin-right: 0.25rem; padding: 0 0.5rem; color: #271919;`,
         ...params
     );
 }
