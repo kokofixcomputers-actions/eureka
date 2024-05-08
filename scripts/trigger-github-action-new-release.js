6cf0e30550b8539485c1e6230a7812863cd74a3b
kokofixcomputers <github@kokofixcomputers.serv00.net>
2024-05-07 18:47:44 -0700

    add a auto publish from terminal command

---
+++ scripts/trigger-github-action-new-release.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const process = require('node:process');
+
+async function triggerWorkflow(owner, repo, workflowId, ref, inputs) {
+  try {
+    const response = await axios.post(
+      `https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflowId}/dispatches`,
+      {
+        ref,
+      },
+      {
+        headers: {
+          'Accept': 'application/vnd.github+json',
+          'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN,
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }
+    );
+
+    console.log('Workflow triggered successfully:', response.status);
+  } catch (error) {
+    console.error('Error triggering workflow:', error.response.data);
+  }
+}
+
+// Example usage
+triggerWorkflow(
+  'kokofixcomputers',
+  'eureka',
+  'validate.yml',
+  'trunk', // The git reference for the workflow (branch or tag name)
+  {
+    triggered_by: 'GitHub API'
+  }
+);
